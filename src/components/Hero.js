b102c93999b99cdeea69bd833a787c83abff156c
Nicknyr <nick@Nicks-MacBook-Pro.local>
2020-02-02 18:16:35 -0500

    Added images

---
+++ src/components/Hero.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import Background from '../assets/background-big.png';
+
+const STYLES = styled.div`
+    background: url(${Background});
+    background-size: cover;
+    height: 100vh;
+`;
+
+
+const Hero = () => {
+    return (
+        <STYLES>
+            
+        </STYLES>
+    );
+}
+
+export default Hero;
